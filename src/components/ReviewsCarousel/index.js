6f1a677542e893f101c7d2f46d7b73cae9e8cdd4
navya-22101999 <navyatammanaboina@gmail.com>
2023-07-17 16:18:00 +0000

    reviews

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,64 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {activeReviewIndex: 0}
+
+  getReview = reviewsList => {
+    const {activeReviewIndex} = this.state
+    const {imgUrl, username, companyName, description} = reviewsList[
+      activeReviewIndex
+    ]
+    return (
+      <div>
+        <h1>Reviews</h1>
+        <img src={imgUrl} alt={username} />
+        <p>{username}</p>
+        <p>{companyName}</p>
+        <p>{description}</p>
+      </div>
+    )
+  }
+
+  onLeft = () => {
+    const {activeReviewIndex} = this.state
+    if (activeReviewIndex > 0) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex - 1,
+      }))
+    }
+  }
+
+  onRight = () => {
+    const {activeReviewIndex} = this.state
+    const {reviewsList} = this.props
+    if (activeReviewIndex < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex + 1,
+      }))
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    return (
+      <div>
+        <button type="button" onClick={this.onLeft} data-testid="leftArrow">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+            alt="left arrow"
+          />
+        </button>
+        {this.getReview(reviewsList)}
+        <button type="button" onClick={this.onRight} data-testid="rightArrow">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+            alt="right arrow"
+          />
+        </button>
+      </div>
+    )
+  }
+}
+export default ReviewsCarousel
